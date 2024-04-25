d931900ecb92c986ffd2bff0e8654fe6ad1343ad
Taranveer Singh <154905330+Taran677@users.noreply.github.com>
2024-04-25 10:14:27 +0530

    minor changes

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,11 +1,26 @@
+import { useState, useEffect } from "react";
 import SvgLogo from "../assets/SvgLogo";
 import Styles from "./Login.module.css";
 import LoginUsing from "./LoginUsing";
 import Signin from "./Signin";
+
 function Login() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
       <div className={Styles["main"]}>
+        <div className={Styles["body-before"]}>
+          <img src="" alt="" />
+        </div>
         <div className={Styles["body"]}>
           <div className={Styles["main-body"]}>
             <div className={Styles["logo-div"]}>
@@ -14,8 +29,8 @@ function Login() {
             <div className={Styles["login-div"]}>
               <div className={Styles["main-heading"]}>Happening now</div>
               <div className={Styles["sub-heading"]}>Join today.</div>
-              <LoginUsing></LoginUsing>
-              <Signin></Signin>
+              <LoginUsing />
+              <Signin />
             </div>
           </div>
         </div>
@@ -23,4 +38,5 @@ function Login() {
     </>
   );
 }
+
 export default Login;
